01dfe950f08d62d20ea5bd5520578b536a2653db
TheZyon <alfredo.ingraldo.u@gmail.com>
2023-03-21 12:44:45 +0100

    sistemato endpoint get all statofatture

---
+++ src/main/java/com/SpringBoot_SpringSecurity/runner/StatoFattureRunner.java
@@ -0,0 +1,23 @@
+package com.SpringBoot_SpringSecurity.runner;
+
+import com.SpringBoot_SpringSecurity.service.FattureService;
+import com.SpringBoot_SpringSecurity.service.StatoFatturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatoFattureRunner implements ApplicationRunner {
+
+    @Autowired private StatoFatturaService service;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    var f=service.getAllFattura();
+
+    f.forEach(sfattura->System.out.println(sfattura));
+
+    }
+}
