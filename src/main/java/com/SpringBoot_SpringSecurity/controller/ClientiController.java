60861905e390a7871acf8bb4c614f3250562480c
Giacomo202 <giaby202@gmail.com>
2023-03-21 16:03:30 +0100

    conbtroller clienti

--- src/main/java/com/SpringBoot_SpringSecurity/controller/ClientiController.java
+++ src/main/java/com/SpringBoot_SpringSecurity/controller/ClientiController.java
@@ -1,12 +1,19 @@
 package com.SpringBoot_SpringSecurity.controller;
 
 import com.SpringBoot_SpringSecurity.entity.BeServiceClienti;
+import com.SpringBoot_SpringSecurity.repository.ClientiRepository;
 import com.SpringBoot_SpringSecurity.service.ClientiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
@@ -14,6 +21,7 @@ import java.util.List;
 public class ClientiController {
 
     @Autowired private ClientiService service;
+    @Autowired private ClientiRepository clientiRepository;
 
 
     //create
@@ -25,9 +33,13 @@ public class ClientiController {
 
 
     //read
-    @GetMapping
-    public ResponseEntity<List<BeServiceClienti>> getAllClienti(){
-        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    @GetMapping("/page/{page}")
+    public Page<BeServiceClienti> getAllClienti(@PathVariable int page,
+    		@RequestParam(defaultValue = "10") int size,
+    		  @RequestParam(defaultValue = "id") String sortBy) {
+        Pageable sortedByName = PageRequest.of(page, size, Sort.by("nomeContatto"));
+        return clientiRepository.findAll(sortedByName);
+        //return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
@@ -39,7 +51,34 @@ public class ClientiController {
     
 
     //delete
-
-
-
+    
+    
+    //query fatturato annuale
+    @GetMapping("/fatturato/{fatturato_annuale}")
+    public ResponseEntity<List<BeServiceClienti>> getByFatturato(@PathVariable BigDecimal fatturato_annuale){
+    	return new ResponseEntity<>(service.cercaTramiteFatturato(fatturato_annuale),HttpStatus.OK);
+    	
+    }
+    
+    //query nome partiale
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<BeServiceClienti>> getByNome(@PathVariable String nome){
+    	return new ResponseEntity<>(service.cercaTramiteParteNome(nome),HttpStatus.OK);
+    }
+    
+    //query ultimo contatto
+    @GetMapping("/contatto/{data}")
+    public ResponseEntity<List<BeServiceClienti>> getByUltimoContatto(@PathVariable Timestamp data){
+    	return new ResponseEntity<>(service.cercaTramiteDataUltimoContatto(data),HttpStatus.OK);
+    }
+    
+    //query data inserimento
+    @GetMapping("/inserimento/{data}")
+    public ResponseEntity<List<BeServiceClienti>> getByDataInserimento(@PathVariable Timestamp data){
+    	return new ResponseEntity<>(service.cercaTramiteInserimentoData(data),HttpStatus.OK);
+    }
 }
+
+
+
+
--- src/main/java/com/SpringBoot_SpringSecurity/repository/ClientiRepository.java
+++ src/main/java/com/SpringBoot_SpringSecurity/repository/ClientiRepository.java
@@ -7,17 +7,17 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientiRepository extends CrudRepository<BeServiceClienti, Long> {
+public interface ClientiRepository extends JpaRepository<BeServiceClienti, Long>{
 
-	
-	
 public List<BeServiceClienti> findByFatturatoAnnuale(BigDecimal fatturatoAnnuale);
-public List<BeServiceClienti>  findByDataInserimento(Timestamp dataInserimento);
-public List<BeServiceClienti>  findByDataUltimoContatto(Timestamp dataUltimoContatto);
+public List<BeServiceClienti> findByDataInserimento(Timestamp dataInserimento);
+public List<BeServiceClienti> findByDataUltimoContatto(Timestamp dataUltimoContatto);
 public List<BeServiceClienti> findByNomeContattoContaining(String nomeContatto);
 
 }
